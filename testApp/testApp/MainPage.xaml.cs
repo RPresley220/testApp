6c0f65a820fd5fd4c850ba1b648fe19565272859
RPresley220 <PreslRichard@gmail.com>
2016-04-25 22:02:32 -0400

    addind to github

---
+++ testApp/testApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace testApp
+{
+    public partial class FirstPage : ContentPage
+    {
+        public Contacts TheContact { get; set; }
+        public FirstPage()
+        {
+            TheContact = new Contacts();
+            TheContact.FirstName = "Richard";
+            TheContact.LastName = "Presley";
+            TheContact.Email = "RP@Gmail.com";
+            BindingContext = TheContact;
+            InitializeComponent();
+        }
+    }
+}
